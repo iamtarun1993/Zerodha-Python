7c417f83d30102baae1ad43bb43ac049c34e1c40
Tarun Kumar Pal <iamtarun1993@gmail.com>
2018-05-04 18:33:59 +0530

    Third commit

--- Bse_zerodha.py
+++ Bse_zerodha.py
@@ -2,6 +2,7 @@ import pandas as pd
 import urllib
 import zipfile
 import datetime
+import webbrowser
 from datetime import timedelta
 pd.set_option('display.expand_frame_repr', False)
 
@@ -12,8 +13,8 @@ def download_zip(filename,url):
 
 def unzip_file_zip(filename):
 	zip_ref = zipfile.ZipFile(filename+'.zip', 'r')
-	zip_ref.extractall()
-	zip_ref.close()
+	zip_ref.extractall();
+	zip_ref.close();
 	csv_data_read(filename)
 
 def csv_data_read(filename):
@@ -23,45 +24,52 @@ def csv_data_read(filename):
 	#print(data_csv_frame);
 	data_csv_frame['CHANGE_PER'] = ((data_csv_frame.CLOSE - data_csv_frame.OPEN)/data_csv_frame.OPEN)*100;
 	print("\nTop 10 Profit Stocks are :")
-	print data_csv_frame.nlargest(10, 'CHANGE_PER');
+	top_10_profit_stock=data_csv_frame.nlargest(10, 'CHANGE_PER');
+	top_10_profit_stock.drop(['OPEN','CLOSE','LOW','HIGH'], axis = 1, inplace = True)
+	print top_10_profit_stock;
 	print("\nTop 10 Loss Stocks are :")
-	print data_csv_frame.nsmallest(10, 'CHANGE_PER')
-	# print(data_csv_frame);
-	# print (data_csv_frame.loc[data_csv_frame['CHANGE_PER'].idxmax()])
-	# print (data_csv_frame.iloc[data_csv_frame['CHANGE_PER'].idxmax()])
-	# print (data_csv_frame['CHANGE_PER'].max(10))
-	#change=[];
-	# for row in data_csv_frame['OPEN','CLOSE']:
-	# 	print(row)
-		#change.append()
-
+	top_10_loss_stock=data_csv_frame.nsmallest(10, 'CHANGE_PER');
+	top_10_loss_stock.drop(['OPEN','CLOSE','LOW','HIGH'], axis = 1, inplace = True)
+	print top_10_loss_stock;
 	#find_stock_info(data_csv_frame);
 
 def find_stock_info(data_csv_frame):
-	ifsc_code=raw_input("Enter 11 digit IFSC Code: ");
+	stock_code=input("Enter 6 digit Stock Code: ");
+	stock_full_info=data_csv_frame[data_csv_frame["SC_CODE"] == stock_code];
+	print(stock_full_info);
+	# f = open('topstock.html','w')
+	# f.write("""<html>
+	# <head></head>
+	# <body><table><tbody>""")
+	# for i in range(0, len(top_10_profit_stock)):
+	# 	f.write("<tr><td>"+str(top_10_profit_stock.iloc[i]['SC_NAME'])+"</td></tr>")
+	
+	# f.write("""</tbody></table></body>
+	# </html>""")
+	# f.close()
 
-
+	# webbrowser.open_new_tab('topstock.html')
 
 def file_date_avail(newday):
 	# print(str(newday.year).zfill(2));
 	# print (newday.strftime("%y"))
 	filename='EQ'+str(newday.day).zfill(2)+str(newday.month).zfill(2)+str(newday.strftime("%y"))
-	print(filename)
+	#print(filename)
 	#filename='EQ300418'
 	url = 'https://www.bseindia.com/download/BhavCopy/Equity/'+filename+'_CSV.ZIP'
 	ret = urllib.urlopen(url)
 	if (ret.code == 200):
-		print("File Found for "+str(newday));
+		print("File Found for "+str(newday.strftime('%d/%m/%Y')));
 		download_zip(filename,url);
 	else:
-		print("No file found for "+str(newday));
+		print("No file found for "+str(newday.strftime('%d/%m/%Y')));
 		newday=newday-timedelta(1);
 		file_date_avail(newday);
 
 
 def main():
 	today = datetime.datetime.now()
-	print(today);
+	#print(today);
 	file_date_avail(today);
 	
 
